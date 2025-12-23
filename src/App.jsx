ff4f88b795f1d75cd6b76db9a8b29c2db1c1e2c4
Subhasis Panda <subhasispanda1256700@gmail.com>
2025-12-23 09:06:46 +0530

    Improved UI transitions, app layout and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import MicrophoneTest from './components/MicrophoneTest';
+import WisprFlow from './components/Deepgram';
 import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
-      <MicrophoneTest />
-    </div>
+    <WisprFlow />
   );
 }
 
